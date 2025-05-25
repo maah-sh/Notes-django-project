9e740f7b9d51132231bc71703aaa78e135d4cf9a
Mahnoosh <my.lovely.moon.333@gmail.com>
2025-05-26 01:07:20 +0330

    model Note created_by field changed to owner

--- notes_project/notes/models.py
+++ notes_project/notes/models.py
@@ -6,7 +6,7 @@ class Note(models.Model):
     title = models.CharField(max_length=200, blank=True, default='')
     content = models.TextField()
     published = models.BooleanField(default=False)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notes', on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notes', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     

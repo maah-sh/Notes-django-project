62536acb91264293cc567e8e094aa8e83c068246
Mahnoosh <my.lovely.moon.333@gmail.com>
2025-05-26 12:44:01 +0330

    delete unnecessary imports in views

--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -1,13 +1,10 @@
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 from .models import Note
 from .serializers import NoteSerializer
-from .permissions import IsOwner, IsOwnerOrReadOnlyPublished
+from .permissions import IsOwnerOrReadOnlyPublished
 
 
 class UserNotesList(generics.ListAPIView):
